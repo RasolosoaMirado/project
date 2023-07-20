e9834f81c82803a7bcc6fcb2c48e4953044b0bc2
RasolosoaMirado <miradoleagueoflegends@gmail.com>
2023-07-20 23:41:07 +0300

    ceci est mon premier push

---
+++ scripts/login.js
@@ -0,0 +1,4 @@
+document.getElementById("login").addEventListener("click", function() {
+    document.querySelector(".signin").style.display = "flex";
+    document.querySelector(".signup").style.display = "none";
+});
